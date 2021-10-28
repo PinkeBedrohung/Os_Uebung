b9037804e8eeb672c0a78f9f110656676313060e
Dzevad Coralic <dzevad.coralic@student.tugraz.at>
2021-10-28 16:28:08 +0200

    pthread_cancel - possible race cond. by thread creation

---
+++ userspace/tests/pthreadcancel.c
@@ -0,0 +1,26 @@
+#include "pthread.h"
+#include "stdio.h"
+#include "assert.h"
+
+void* CanceledThread()
+{
+    printf("We are in the canceled thread!\n");
+    while(1){}
+    return 0;
+}
+
+
+int main()
+{
+    pthread_t cancd_thread;
+
+    pthread_create(&cancd_thread, 0x0, &CanceledThread, 0x0);
+    for(int i = 0; i < 99999999; i++)
+    {
+
+    }
+    
+    int cancel_retval = pthread_cancel(cancd_thread);
+    printf("Thread canceled with retval: %d\n", cancel_retval);
+    return 0;
+}
