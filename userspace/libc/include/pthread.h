e23479c36d5e27f95977138831cc28749e5ae368
Dzevad Coralic <dzevad.coralic@student.tugraz.at>
2021-11-18 19:19:09 +0100

    adding cancelation state/type enums

--- userspace/libc/include/pthread.h
+++ userspace/libc/include/pthread.h
@@ -9,7 +9,20 @@ extern "C" {
 //pthread typedefs
 typedef size_t pthread_t;
 typedef unsigned int pthread_attr_t;
-
+enum
+{
+  PTHREAD_CANCEL_ENABLE,
+#define PTHREAD_CANCEL_ENABLE   PTHREAD_CANCEL_ENABLE
+  PTHREAD_CANCEL_DISABLE
+#define PTHREAD_CANCEL_DISABLE  PTHREAD_CANCEL_DISABLE
+};
+enum
+{
+  PTHREAD_CANCEL_DEFERRED,
+#define PTHREAD_CANCEL_DEFERRED	PTHREAD_CANCEL_DEFERRED
+  PTHREAD_CANCEL_ASYNCHRONOUS
+#define PTHREAD_CANCEL_ASYNCHRONOUS	PTHREAD_CANCEL_ASYNCHRONOUS
+};
 //pthread mutex typedefs
 typedef unsigned int pthread_mutex_t;
 typedef unsigned int pthread_mutexattr_t;
